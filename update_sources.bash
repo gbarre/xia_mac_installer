e4b3d17b1049ea4f3ab9a1f51989d1523ee6b05d
Guillaume Barré <guillaume.barre@ac-versailles.fr>
2016-10-25 13:47:35 +0200

    add update_sources script

---
+++ update_sources.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SOURCE="/Users/guillaume/Documents/xia/xia"
+INSTALLER="/Users/guillaume/Documents/xia/xia_mac_installer"
+FILES="$INSTALLER/Xia_Installer.app/Contents/Resources/files"
+
+if [ -d $SOURCE ]; then
+	cd "$SOURCE"
+	git pull
+	cd src
+fi
+
+if [ -d $FILES ]; then
+	cp -r xiaconverter $FILES
+	cp -r share $FILES
+	cp xia.cnf $FILES
+	cp inkscape-plugin/xia* $FILES
+fi
+
+if [ -e $INSTALLER/Xia_Installer_old.app.zip ]; then
+	rm -f $INSTALLER/Xia_Installer_old.app.zip
+fi
+
+mv $INSTALLER/Xia_Installer.app.zip $INSTALLER/Xia_Installer_old.app.zip
+
+cd $INSTALLER
+zip -r Xia_Installer.app.zip Xia_Installer.app
+
+echo "Sources files updated"
+exit 0
